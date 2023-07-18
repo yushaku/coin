4a9314bda7c246207fd751411c530e7f0f442f2c
yushaku <levanson180200@gmail.com>
2023-07-18 14:29:07 +0700

    update

---
+++ apps/hardhat/scripts/deploy.zombie.ts
@@ -0,0 +1,15 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  const token = await ethers.deployContract("CryptoZombies");
+  console.log("Token CryptoZombies address:", await token.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
